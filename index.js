48513adc56fa4c9d3b6478d6ceaac2fc47d37156
Santiago Ferreira <santiago.ferreira@wyeworks.com>
2016-07-19 20:54:48 -0300

    Use source-map-support to show the stacktrace

--- index.js
+++ index.js
@@ -2,5 +2,21 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-qunit-source-map'
+  name: 'ember-qunit-source-map',
+
+  options: {
+    nodeAssets: {
+      "source-map-support": function() {
+        return {
+          enabled: this.app.env === 'test',
+          import: ['browser-source-map-support.js']
+        };
+      }
+    }
+  },
+  contentFor: function(type, config) {
+    if (type === 'test-body-footer') {
+      return '<script>require("browser-source-map-support").install();</script>';
+    }
+  }
 };
